e0d83668a3e3a62c9d6dd3d6ca55b61464863dce
littlehorseboy <littlehorseboy@gmail.com>
2020-04-13 14:36:50 +0800

    test: 測試注入 UserService 的 welcome.component

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,13 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeComponent } from './welcome.component';
+import { UserService } from '../user.service';
+
+class MockUserService {
+  isLoggedIn = true;
+  user = { name: 'Test User' };
+}
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
+  let userService: UserService ;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers: [
+        { provide: UserService, useClass: MockUserService },
+      ],
       declarations: [ WelcomeComponent ]
     })
     .compileComponents();
@@ -16,10 +26,30 @@ describe('WelcomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
+    userService = TestBed.inject(UserService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not have welcome message after construction', () => {
+    expect(component.welcome).toBeUndefined();
+  });
+
+  it('should welcome logged in user after Angular calls ngOnInit', () => {
+    component.ngOnInit();
+
+    expect(component.welcome).toContain(userService.user.name);
+  });
+
+  it('should ask user to log in if not logged in after ngOnInit', () => {
+    userService.isLoggedIn = false;
+
+    component.ngOnInit();
+
+    expect(component.welcome).not.toContain(userService.user.name);
+    expect(component.welcome).toContain('log in');
   });
 });
