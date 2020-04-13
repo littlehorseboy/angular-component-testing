9b2a0f98855d4258819de0434dadba0788fa502e
littlehorseboy <littlehorseboy@gmail.com>
2020-04-13 14:16:44 +0800

    feat: welcome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LightswitchComponent } from './lightswitch/lightswitch.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LightswitchComponent
+    LightswitchComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
